f7cac23d2f8456e486764a2d8eb3a1425411fabb
Josh Pittman <code@joshpittman.com>
2021-10-30 11:53:57 -0400

    introduced floating labels to register form

--- client/src/pages/register.js
+++ client/src/pages/register.js
@@ -1,4 +1,4 @@
-import { Form, Button } from "react-bootstrap";
+import { Form, Button, FloatingLabel } from "react-bootstrap";
 import { register } from "../components/dal";
 import useInput from "../hooks/useInput";
 
@@ -32,29 +32,62 @@ export default function Register() {
         <Form.Control placeholder="First" {...fname} required/>
         <Form.Control placeholder="Last" {...lname} required/>
       </Form.Group> */}
-      <Form.Label>Name</Form.Label>
+
       <Form.Group>
-          <input placeholder="First" autoComplete="given-name" {...fname} required />
-        
+        <FloatingLabel controlId="floatingInput" label="First Name" className="mb-3">
+          <input
+            placeholder="First Name"
+            autoComplete="given-name"
+            {...fname}
+            className="form-control mb-3"
+            required
+          />
+        </FloatingLabel>
+        <FloatingLabel controlId="floatingInput" label="Last Name" className="mb-3">
+          <input
+            placeholder="Last Name"
+            autoComplete="given-name"
+            {...lname}
+            className="form-control mb-3"
+            required
+          />
+        </FloatingLabel>
         <br />
-        <input placeholder="Last" autoComplete="family-name" {...lname} required />
+        <input
+          placeholder="Last"
+          autoComplete="family-name"
+          {...lname}
+          required
+        />
         <br />
       </Form.Group>
       <Form.Label>Email</Form.Label>
       <Form.Group>
-      <input type="email" placeholder="Your Email" autoComplete="email" {...email} required />
+        <input
+          type="email"
+          placeholder="Your Email"
+          autoComplete="email"
+          {...email}
+          required
+        />
       </Form.Group>
       <Form.Label>Password</Form.Label>
       <Form.Group>
-      <input placeholder="Password" type="password" autoComplete="new-password" {...password} required />
-      <br />
-      <input
-        placeholder="Confirm"
-        type="password"
-        autoComplete="new-password"
-        {...confirmPassword}
-        required
-      />
+        <input
+          placeholder="Password"
+          type="password"
+          autoComplete="new-password"
+          {...password}
+          required
+        />
+        <br />
+        <input
+          placeholder="Confirm"
+          type="password"
+          autoComplete="new-password"
+          {...confirmPassword}
+          required
+        />
       </Form.Group>
       <Button type="submit">Register</Button>
     </Form>
