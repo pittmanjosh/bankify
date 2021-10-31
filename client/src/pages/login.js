e26087478a0faf6fdf1d90c7f09a9c5bd17d958e
Josh Pittman <code@joshpittman.com>
2021-10-30 21:27:46 -0400

    removed login page card

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -55,7 +55,6 @@ export default function Login() {
             <Button type="submit">Register</Button>
           </Form>
         </Card.Body>
-        <Card.Footer/>
       </Card>
     </Container>
   );
