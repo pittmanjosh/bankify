3f2f710b0c519a29372cb8b259c7610b3a348363
Josh Pittman <code@joshpittman.com>
2021-11-01 13:57:21 -0400

    deconstructed firebase admin config

--- firebase/admin.js
+++ firebase/admin.js
@@ -1,10 +1,31 @@
-const { initializeApp } = require('firebase-admin/app');
+const admin = require('firebase-admin');
+  
+const type = process.env.GOOGLE_CERT_TYPE
+const project_id = process.env.GOOGLE_CERT_PROJECT_ID
+const private_key_id = process.env.GOOGLE_CERT_KEY_ID
+const private_key = process.env.GOOGLE_CERT_KEY
+const client_email = process.env.GOOGLE_CERT_EMAIL
+const client_id = process.env.GOOGLE_CERT_CLIENT_ID
+const auth_uri = process.env.GOOGLE_CERT_AUTH_URI
+const token_uri = process.env.GOOGLE_CERT_TOKEN_URI
+const auth_provider_x509_cert_url = process.env.GOOGLE_CERT_AUTH_CERT_URL
+const client_x509_cert_url = process.env.GOOGLE_CERT_CLIENT_CERT_URL
 
-var app;
 
-if (!app){
-  app = initializeApp(process.env.GOOGLE_CONFIG || require('../.bankify.json'))
-}
+var app = admin.initializeApp({
+  credential: admin.credential.cert({
+    type,
+    project_id,
+    private_key_id,
+    private_key.replace(/\\n/g,'\n'),
+    client_email,
+    client_id,
+    auth_uri,
+    token_uri,
+    auth_provider_x509_cert_url,
+    client_x509_cert_url
+  })
+})
 
 if (app) {
   console.log("ADMIN Configured")
