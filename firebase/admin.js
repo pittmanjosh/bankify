6f65d9ab36b8e8a52f97d4d4f5be675548d7ed12
Josh Pittman <code@joshpittman.com>
2021-11-01 14:05:06 -0400

    removed keys

--- firebase/admin.js
+++ firebase/admin.js
@@ -1,15 +1,15 @@
 const admin = require('firebase-admin');
   
-const type = process.env.GOOGLE_CERT_TYPE
-const project_id = process.env.GOOGLE_CERT_PROJECT_ID
-const private_key_id = process.env.GOOGLE_CERT_KEY_ID
-const private_key = process.env.GOOGLE_CERT_KEY
-const client_email = process.env.GOOGLE_CERT_EMAIL
-const client_id = process.env.GOOGLE_CERT_CLIENT_ID
-const auth_uri = process.env.GOOGLE_CERT_AUTH_URI
-const token_uri = process.env.GOOGLE_CERT_TOKEN_URI
-const auth_provider_x509_cert_url = process.env.GOOGLE_CERT_AUTH_CERT_URL
-const client_x509_cert_url = process.env.GOOGLE_CERT_CLIENT_CERT_URL
+const type = process.env.GOOGLE_CERT_TYPE;
+const project_id = process.env.GOOGLE_CERT_PROJECT_ID;
+const private_key_id = process.env.GOOGLE_CERT_KEY_ID;
+const private_key = process.env.GOOGLE_CERT_KEY.replace(/\\n/g,'\n');
+const client_email = process.env.GOOGLE_CERT_EMAIL;
+const client_id = process.env.GOOGLE_CERT_CLIENT_ID;
+const auth_uri = process.env.GOOGLE_CERT_AUTH_URI;
+const token_uri = process.env.GOOGLE_CERT_TOKEN_URI;
+const auth_provider_x509_cert_url = process.env.GOOGLE_CERT_AUTH_CERT_URL;
+const client_x509_cert_url = process.env.GOOGLE_CERT_CLIENT_CERT_URL;
 
 
 var app = admin.initializeApp({
@@ -17,7 +17,7 @@ var app = admin.initializeApp({
     type,
     project_id,
     private_key_id,
-    private_key.replace(/\\n/g,'\n'),
+    private_key,
     client_email,
     client_id,
     auth_uri,
@@ -25,7 +25,7 @@ var app = admin.initializeApp({
     auth_provider_x509_cert_url,
     client_x509_cert_url
   })
-})
+});
 
 if (app) {
   console.log("ADMIN Configured")
