931c5e8d354db105ff5f0505ccbf2efeba140ea1
Josh Pittman <code@joshpittman.com>
2021-11-03 13:33:32 -0400

    added framework for alldata

--- client/src/pages/alldata.js
+++ client/src/pages/alldata.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { useEffect,useState } from 'react'
+import {useSetAlert} from '../hooks/useAlert';
 
 export default function AllData() {
-  return (
-    <div>
-      All Data
-    </div>
-  )
+  const [data,setData] = useState(null);
+
+  useEffect(()=>{
+    var requestOptions = { method: 'GET', redirect: 'follow' };
+    
+    fetch("/backdoor", requestOptions)
+      .then(response => response.text())
+      .then(result => setData(JSON.stringify(result)))
+      .catch(error => {
+        setData('');
+      });
+  })
+  
+
+  return <div>{data ? data : "loading..."}</div>
 }
