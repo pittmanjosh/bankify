27e55689b4e96ac188bd25f1a74cf2c4b35dda37
Josh Pittman <code@joshpittman.com>
2021-11-08 12:48:37 -0500

    made auth synchronus

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,30 +1,26 @@
 const fbAdmin = require('../firebase/admin');
 
-async function authentication(req,res,next) {
+function authentication(req,res,next) {
   // read token from header
-  (async ()=>{
-    const idToken = req.headers.authorization?.split(' ')[1] || null;
+  const idToken = req.headers.authorization?.split(' ')[1] || null;
   
-    if (idToken) {
-      fbAdmin.auth()
-        .verifyIdToken(idToken)
-        .then((decodedToken)=>{
-          const {uid, email, name, picture} = decodedToken;
-          req.uid = uid;
-          req.email = email;
-          req.name = name;
-          req.photoURL = picture;
-          console.log("Authorized User:",uid);
-          next();
-        })
-        .catch(()=>{
-          res.status(401).send("Authentication failed")
-        })
-    } else {
-      res.status(401).send("Missing idToken")
-    }
-  })();
-    
+  if (idToken) {
+    fbAdmin.auth().verifyIdToken(idToken)
+    .then((decodedToken)=>{
+      const {uid, email, name, picture} = decodedToken;
+      req.uid = uid;
+      req.email = email;
+      req.name = name;
+      req.photoURL = picture;
+      console.log("Authorized User:",uid);
+      next();
+    })
+    .catch(()=>{
+      res.status(401).send("Authentication failed")
+    })
+  } else {
+    res.status(401).send("Missing idToken")
+  }
 };
 
 module.exports = authentication;
