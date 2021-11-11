e8c2fdb533c6e627757c1554c3d356c28a4a864c
Josh Pittman <code@joshpittman.com>
2021-11-11 16:53:06 -0500

    imported register from adapter in new register page

--- client/src/pages/register.js
+++ client/src/pages/register.js
@@ -6,7 +6,7 @@ import {
   FloatingLabel,
   Form,
 } from "react-bootstrap";
-import { loginEmailPassword, loginGoogle } from "../adapters/firebase";
+import { register, loginGoogle } from "../adapters/firebase";
 import useInput from "../hooks/useInput";
 import ctx from "../context";
 import { useContext } from "react";
