f98033298f772b3b9e2bf04043e3a644de83e36d
Josh Pittman <code@joshpittman.com>
2021-11-11 14:24:38 -0500

    removed currentUser from register

--- client/src/adapters/firebase.js
+++ client/src/adapters/firebase.js
@@ -43,7 +43,6 @@ export function loginGoogle(createAlert) {
 }
 
 export function register(name, email, pwd, createAlert) {
-  const currentUser = auth.currentUser();
   createUserWithEmailAndPassword(auth, email, pwd)
     .then((user) => {
       createAlert("You are now registered!", "success");
@@ -56,7 +55,8 @@ export function register(name, email, pwd, createAlert) {
       })
         .then(x=>console.log("update profile result:",x))
         .catch((x) => createAlert(x.message, "danger", "Name not filed!"));
-      console.log("register current",currentUser);
+        
+      console.log(user);
       createUser(user);
     })
     .catch((x) => createAlert(x.message, "danger", "Registration Failed!"));
